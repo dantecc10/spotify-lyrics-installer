dbf61f7721cc9a6e5afb59d129687b3b3ab88743
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-11 18:59:15 -0600

    added first.sh script

---
+++ first.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/darkseid/scripting/spotify-lyrics/
+
+python3 -m venv env
+source env/bin/activate
+
+pip3 install -r requirements.txt
+
+# Correr
+# python3 SpotifyLyrics.pyw
+
+# Correr y dejar proceso en segundo plano (tiene una molesta salida constante)
+python3 SpotifyLyrics.pyw &
